388a9a7d3964531ca4c254c6743e4112d3bab296
HyunSJ <dudtod1596@gmail.com>
2023-10-13 10:44:39 +0900

    feat : artist 페이지 연관된 artist section 추가

--- src/app/artist/[slug]/page.tsx
+++ src/app/artist/[slug]/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import AlbumCardContentList from '@/components/AlbumCardContentList'
+import ContentCardListItem from '@/components/CardContentListItem'
 import Banner from '@/components/common/Banner'
+import ContentCardContainer from '@/components/common/ContentContainer'
 import GlobalPlayButton from '@/components/common/GlobalPlayButton'
 import TrackContainer from '@/components/common/trackItem/TrackContainer'
 import getArtist from '@/core/api/artist/getArtist'
 import getArtistTopsTrack from '@/core/api/artist/getArtistTopTrack'
+import getRelatedArtists from '@/core/api/artist/getRelatedArtists'
+import { createContentFromItem } from '@/lib/utils/createContentObject'
 import { GetArtist } from '@/types/raw-api-data-type/artist/get-artist-data-type'
 import { GetArtistTopTrack } from '@/types/raw-api-data-type/artist/get-artist-top-track-data-type'
 import { GetArtistsAlbums } from '@/types/raw-api-data-type/artist/get-artists-albums-data-type'
+import { GetRelatedArtists } from '@/types/raw-api-data-type/artist/get-related-artist-data-type'
 
 import getArtistAlbums from '../../../core/api/artist/getArtistAlbums'
 
@@ -16,6 +21,8 @@ export default async function page({ params }: { params: { slug: string } }) {
   const artist: GetArtist = await getArtist(params.slug)
   const topTracks: GetArtistTopTrack = await getArtistTopsTrack(params.slug)
   const albums: GetArtistsAlbums = await getArtistAlbums(params.slug, 8)
+  const relatedArtists: GetRelatedArtists = await getRelatedArtists(params.slug)
+
   return (
     <>
       <Banner
@@ -39,6 +46,11 @@ export default async function page({ params }: { params: { slug: string } }) {
         <TrackContainer className={''} tracks={topTracks.tracks} />
       </div>
       <AlbumCardContentList title={'디스코그래피'} linkPath={''} albums={albums.items} />
+      <ContentCardContainer title={`팬들이 좋아하는 다른 음악`} linkPath={'/test'}>
+        {relatedArtists?.artists.map((artist) => (
+          <ContentCardListItem key={artist.id} content={createContentFromItem.artist(artist)} />
+        ))}
+      </ContentCardContainer>
     </>
   )
 }
