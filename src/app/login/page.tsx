f95ed64541b9478e82442d4ac5c583cf6a4cbc08
HyunSJ <dudtod1596@gmail.com>
2023-10-13 10:03:24 +0900

    feat : main artist login track 페이지 metadata 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next'
 import React from 'react'
 
+export const metadata: Metadata = {
+  title: 'Team-C Spotify Web player | Login',
+  description: 'Login page in Team-C Spotify Web player',
+}
 import LoginButton from '@/components/LoginButton'
 
 export default function page() {
